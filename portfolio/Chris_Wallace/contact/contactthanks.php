64d970400e80c9aebe799ca83d4f9a09694b60ce
rachelhavas <rachel.havas2@gmail.com>
2020-01-28 12:07:39 -0500

    adding project to account

---
+++ portfolio/Chris_Wallace/contact/contactthanks.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
+		<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+		<title>Wallace for Ohio</title>
+		<meta content="The official campaign website of state representative candidate Chris Wallace." name="description"/>
+		<meta content="Chris Wallace, Wallace for Ohio, Ohio state representative candidate, Ohio house candidate" name="keywords"/>
+		<link href='' rel='stylesheet' type='text/css'>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"/>
+		<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet"/>
+		<link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet"/>
+		<link href="https://fonts.googleapis.com/css?family=Andada" rel="stylesheet"/>
+		<link href="../css/style.css" rel="stylesheet"/>
+	</head>
+	<body>
+  	<header>
+			<div class="header-wrap">
+				<nav>
+					<ul>
+						<li><a href="../index.html">Home</a></li>
+						<li><a href="../about/index.html">Meet Chris</a></li>
+						<li><a href="../issues/index.html">Issues</a></li>
+						<li><a class="logo" href="../index.html"><img src="../images/logo.jpg"/></a></li>
+						<li><a href="../volunteer/index.html">Volunteer</a></li>
+						<li><a href="../donate/index.html">Donate</a></li> 
+						<li class="current"><a href="../contact/index.html">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+		<div class="container">
+			<main>
+				<!--<div id="contact-banner"></div>-->
+				<section id="contact">
+					<div class="contact-wrap">
+						<h1 class="thanks">Your message has been sent!</h1>
+						<!--<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script>
+						<script type="text/javascript"> _uacct = "UA-68528-29"; urchinTracker();</script>-->
+					</div>
+				</section>
+			</main>
+		</div>
+		<footer>
+			<div class="wrap2">
+				<div class="left">
+					<img class="logo" src="../images/footer-logo.png">
+					<p class="copyright">Paid for by Wallace for Ohio</p>
+				</div>
+				<div class="right">
+					<ul class="links">
+						<li><a href="../index.html">Home</a></li>
+						<li><a href="../about/index.html">Meet Chris</a></li>
+						<li><a href="../issues/index.html">Issues</a></li>
+						<li><a href="../volunteer/index.html">Volunteer</a></li>
+						<li><a href="../donate/index.html">Donate</a></li> 
+						<li><a href="../contact/index.html">Contact</a></li>
+					</ul>
+					<p class="members">Made possible by the Official Members of Team Chris</p>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
---
+++ portfolio/Serenity_Laundry/contact/contactthanks.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible"/>
+<meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+<title>Serenity Laundry</title>
+<meta content="The official website of Serenity Laundry." name="description"/>
+<meta content="laundry, laundry services, dry cleaning, mobile laundry" name="keywords"/>
+<link href="https://fonts.googleapis.com/css?family=Andada" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet"/>
+<link href="../css/style.css" rel="stylesheet"/>
+<link href="../css/responsive-styles.css" rel="stylesheet"/>
+</head>
+<body>
+<header>
+<div class="header-wrap">
+<nav>
+<div class="center">
+<a class="logo" href="../index.html"><img src="../images/logo.jpg"/></a>
+</div>
+<ul>
+<div class="left">
+<li><a href="../index.html">Home</a></li>
+<li><a href="../about/index.html">About</a></li>
+</div>
+<div class="right">
+<li><a href="../services/index.html">Services</a></li>
+<li class="current"><a href="../contact/index.html">Contact</a></li>
+</div>
+</ul>
+</nav>
+</div>
+</header>
+<div class="container">
+<main>
+<section id="contact">
+<div class="contact-wrap">
+<div class="contact-heading">
+<h1>contact</h1>
+<hr></hr>
+<p>1 load = 2 level laundry baskets or 1 overflowing</p>
+</div>
+<div class="contact-body">
+<h1 class="thanks">Your message has been sent!</h1>
+</div>
+</div>
+</section>
+</main>
+</div>
+<footer>
+<div class="footer-wrap">
+<div id="footer-left">
+<img class="footer-logo" src="../images/footer-logo.png"/>
+<div class="social">
+<!--<ul>
+<li>
+<a class="ss-social" href="https://www.facebook.com/FLUXIA21133">&#xe803;</a>
+<a class="ss-social" href="https://twitter.com/rachel21133">&#xe804;</a>
+<a class="ss-social" href="http://rachel21133.tumblr.com/">&#xe807;</a>
+<a class="ss-social" href="https://www.pinterest.com/rachel21133/">&#xe80f;</a>
+<a class="ss-social" href="https://plus.google.com/u/0/+FLUXIA21133/posts">&#xe806;</a>
+</li>
+</ul>-->
+</div>
+</div>
+<div id="footer-right">
+<ul class="footer-links">
+<li><a href="../index.html">Home</a></li>
+<li><a href="../about/index.html">About</a></li>
+<li><a href="../services/index.html">Services</a></li>
+<li><a href="../contact/index.html">Contact</a></li>
+</ul>
+<p class="copyright">Copyright &copy; 2019 Fluxia</p>
+</div>
+</div>
+</footer>
+</body>
+</html>
